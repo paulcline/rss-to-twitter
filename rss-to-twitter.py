a10869346a6a4c5e48da12e1daf329683f87dc5a
Paul Cline <paulcline@gmail.com>
2014-10-16 11:27:54 -0400

    Shortening URLs with tinyurl

--- rss-to-twitter.py
+++ rss-to-twitter.py
@@ -4,6 +4,7 @@ import os
 import sqlite3
 import sys
 import twitter
+import urllib
 import yaml
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
@@ -44,23 +45,26 @@ try:
 
     char_limit = 140
 
-    # if url > char_limit, don't tweet (todo: bitly?)
-    if len(url) > char_limit:
-      logging.warning("URL: %s is too long; Consider implementing a shortener!" % (url))
-      break
+    # shorten the url 
+    try:
+      shortFile = urllib.urlopen("http://tinyurl.com/api-create.php?url=%s" % url)
+      shortUrl = shortFile.read()
+    except:
+      shortUrl = url
+    finally:
+      shortFile.close()
 
     # build message, if something gets truncated it should be the title
-    message = "%s %s" % (entry["title"][:char_limit-1-len(url)], url)
+    message = "%s %s" % (entry["title"][:char_limit-1-len(shortUrl)], shortUrl)
 
     # insert url into database
     cursor.execute("INSERT INTO recent(url) VALUES (?)", (url,))
 
     db.commit()
-    db.close()
 
     logging.info("Tweeting \"%s\" | length: %s" % (message, len(message)))
 
-    twitterApi.PostUpdate(message)
+    # twitterApi.PostUpdate(message)
 
     break
 
@@ -68,3 +72,7 @@ except Exception as e:
 
   logging.critical(e)
   sys.exit()
+
+finally:
+
+  db.close()
